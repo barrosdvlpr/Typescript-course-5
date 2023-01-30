345dc9ee87cecd65d38be52a8e6edd1a53b6be34
barrosdvlpr <barrosdvlpr@gmail.com>
2023-01-30 09:29:24 -0300

    simplify-iteration-data-structures-with-iterators

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,33 +7,23 @@ export default function Home() {
   )
 }
 
+////////////////////////////////////////////////
 
+let i = 0;
+do {
+  console.log(numbers[i]);
+  i++;
+} while (i < numbers.length);
 
-interface TreeNode<T> {
-  value: T;
-  left: TreeNode<T>;
-  right: TreeNode<T>;
+for (let num of numbers) {
+  console.log(num);
 }
 
-
-
-interface LinkedListNode<T> {
-  value: T;
-  next: LinkedListNode<T>;
-}
-
-
-
-let node: LinkedListNode<string>;
-node.next.next.next.next.next.next.next.next.value;
-
-
+////////////////////////////////////////////////
 
 interface Action {
   type: string;
 }
-
-// action1/state1 --> action2/state2 --> action3/state3
 
 interface ListNode<T> {
   value: T;
@@ -41,35 +31,69 @@ interface ListNode<T> {
   prev: ListNode<T>;
 }
 
+class BackwardsActionIterator implements IterableIterator<Action> {
+  [Symbol.iterator](): IterableIterator<Action> {
+    // throw new Error("Method not implemented.");
+    return this;
+  }
+
+  next(): IteratorResult<Action> {
+    throw new Error("Method not implemented.");
+  }
+}
+
+////////////////////////////////////////////////
+
+class BackwardsActionIterator implements IterableIterator<Action> {
+  constructor(private_currentActionNode: ListNode<Action>){
+
+  }
+  [Symbol.iterator](): IterableIterator<Action> {
+    return this;
+  }
+
+  next(): IteratorResult<Action> {
+    const curr = this._currentActionNode;
+    if(!curr || !curr.value) {
+      return {value: null, done: true};
+    }
+    //1. move through each item in the list 
+    this._currentActionNode = curr.prev;
+    //2. return each item
+    return {value: curr.value, done: false};
+  }
+}
+
 let action1 = { type: "LOGIN" };
 let action2 = { type: "LOAD_POSTS" };
-
+let action3 = { type: "DISPLAY_POSTS" };
+let action4 = { type: "LOGOUT" };
 
 
 let actionNode1: ListNode<Action> = {
-  value: action1,
+  prev: null,
   next: null,
-  prev: null
+  value: action1
 };
-
 let actionNode2: ListNode<Action> = {
-  value: action2,
+  prev: actionNode1,
   next: null,
-  prev: actionNode1
+  value: action2
 };
-
 actionNode1.next = actionNode2;
+let actionNode3: ListNode<Action> = {
+  prev: actionNode2,
+  next: null,
+  value: action3
+};
+actionNode2.next = actionNode3;
+let actionNode4: ListNode<Action> = {
+  prev: actionNode3,
+  next: null,
+  value: action4
+};
+actionNode3.next = actionNode4;
 
-actionNode1.next = actionNode2;
-actionNode1.next.next.next.next.value;
-
-
-
-actionNode1.next = actionNode2;
-
-let currentNode = actionNode2;
-
-do {
-  console.log(currentNode.value);
-  currentNode = currentNode.prev;
-} while (currentNode);
+const backwardsActionsList = new BackwardsActionIterator (
+  actionNode4
+);
